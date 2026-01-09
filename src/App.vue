89198d57772540e24f6c73a48d077807cb18517f
Myrrhwhis <52115743+meehua@users.noreply.github.com>
2026-01-10 00:48:06 +0800

    添加路由动画

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { KeepAlive, Transition } from "vue";
+import { RouterView } from "vue-router";
 import { invoke } from "@tauri-apps/api/core";
 import Sidebar from '@/components/Sidebar.vue';
-
+import { useRoute } from "vue-router";
+const route = useRoute();
 
 </script>
 
 <template>
   <div class="h-dvh w-dvw flex bg-neutral-50 dark:bg-neutral-900">
-  <Sidebar class="sidebar"/>
+    <Sidebar class="sidebar" />
     <main class="main-container">
-      <router-view />
+      <RouterView v-slot="{ Component }">
+        <Transition name="fade" mode="default">
+          <div :key="route.fullPath">
+            <KeepAlive>
+              <component :is="Component" />
+            </KeepAlive>
+          </div>
+        </Transition>
+      </RouterView>
     </main>
   </div>
 
@@ -26,5 +36,16 @@ import Sidebar from '@/components/Sidebar.vue';
 .main-container {
   /* @apply fixed min-h-dvh right-0 top-0 w-2/3 flex bg-cover bg-center bg-no-repeat; */
   @apply flex-1 overflow-auto;
+}
+
+/* 路由切换动画 */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.1s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>

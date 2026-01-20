e5f60e810805c31ba2292a678e02d63fd1b36eb5
Myrrhwhis <52115743+meehua@users.noreply.github.com>
2026-01-20 20:45:48 +0800

    初步添加资源页的相关组件

---
+++ src/components/ResourceTabs.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex gap-2 mb-6 border-b pb-1 border-gray-800">
+        <button
+            class="flex items-center gap-2 px-4 py-3 text-sm font-medium rounded-t-md transition-all hover:bg-gray-800 hover:border-gray-700 hover:cursor-pointer"
+            v-for="item in tabs" :key="item.type" :class="{ 'selected': item.type == selectedTab?.type }"
+            @click="resourceStore.setSelectedTab(item.type)">
+            <!-- 图标 -->
+            <!-- <span :class="item.icon"></span> -->
+            {{ item.name }}
+        </button>
+    </div>
+</template>
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useResourceStore } from '@/stores/useResourceStore';
+import { storeToRefs } from 'pinia';
+const resourceStore = useResourceStore();
+const { tabs, selectedTab } = storeToRefs(resourceStore);
+
+</script>
+<style scoped>
+@reference "tailwindcss";
+
+.selected {
+    @apply bg-blue-500 border-amber-500 border-b-2;
+}
+</style>
--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -3,11 +3,18 @@
         <!-- 顶栏标题 -->
         <TopBar title="Resources"
             subtitle="Manage all game resources uniformly. MeshCraft will automatically mount resources according to the save configuration on startup." />
+        <!-- 资源导航栏 -->
+        <ResourceTabs />
     </div>
 </template>
 
 <script setup>
 import TopBar from '@/components/TopBar.vue';
+import ResourceTabs from '@/components/ResourceTabs.vue';
+import { useResourceStore } from '@/stores/useResourceStore';
+import { storeToRefs } from 'pinia';
+const resourceStore = useResourceStore();
+const { tabs } = storeToRefs(resourceStore);
 </script>
 <style scoped>
 @reference "tailwindcss";
